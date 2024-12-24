ae46cf5b302f9c089bafc9513b985f37669149a0
k240801 <jongwoo.kang@kaongroup.com>
2024-12-24 11:29:07 +0900

    Add Fragments

---
+++ app/src/main/java/com/nongju/commutehelper/fragments/BusFragment.kt
@@ -0,0 +1,8 @@
+package com.nongju.commutehelper.fragments
+
+import androidx.fragment.app.Fragment
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class BusFragment : Fragment() {
+}
